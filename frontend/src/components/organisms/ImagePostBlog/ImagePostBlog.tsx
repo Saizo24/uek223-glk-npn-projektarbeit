56f6414b2a111740a7fb0f4fa62c48cb075c6975
Kunfi <gian-luca.kunfermann@noseryoung.ch>
2022-11-04 13:49:04 +0100

    implemented imagepostservice

--- frontend/src/components/organisms/ImagePostBlog/ImagePostBlog.tsx
+++ frontend/src/components/organisms/ImagePostBlog/ImagePostBlog.tsx
@@ -1,36 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import { ImagePost } from '../../../types/models/ImagePost.model'
-import Box from '@mui/material/Box'
-import ImagePostEntry from '../../molecules/imagePost/ImagePostEntry'
-import MySearchBar from '../../molecules/SearchBar/MySearchBar'
+import React, { useEffect, useState } from "react";
+import { ImagePost } from "../../../types/models/ImagePost.model";
+import Box from "@mui/material/Box";
+import ImagePostEntry from "../../molecules/imagePost/ImagePostEntry";
+import MySearchBar from "../../molecules/SearchBar/MySearchBar";
 
 type Props = {
-    imagePostList: ImagePost[]
-    postsEditable: boolean
-}
+  imagePostList: ImagePost[];
+  postsEditable: boolean;
+};
 
 const ImagePostBlog = ({ imagePostList, postsEditable }: Props) => {
-    const [imagePosts, setImagePosts] = useState(imagePostList)
-    const [searchValue, setSearchValue] = useState("")
+  const [imagePosts, setImagePosts] = useState(imagePostList);
+  const [searchValue, setSearchValue] = useState("");
 
-    useEffect(() => {
-        setImagePosts(imagePostList)
-    }, [imagePostList])
+  useEffect(() => {
+    setImagePosts(imagePostList);
+  }, [imagePostList]);
 
-    return (
-        <Box>
-            <MySearchBar searchItem='User' searchValue={searchValue} setSearchValue={setSearchValue} />
-            {
-                imagePosts.filter((post) => {
-                    return `${post.author.firstName} ${post.author.lastName}`.toLocaleLowerCase().includes(searchValue)
-                }).map((post) => {
-                    return (
-                        <ImagePostEntry imagePost={post} editable={postsEditable} />
-                    )
-                })
-            }
-        </Box>
-    )
-}
+  return (
+    <Box>
+      <MySearchBar
+        searchItem="User"
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+      {imagePosts
+        .filter((post) => {
+          return `${post.author.firstName} ${post.author.lastName}`
+            .toLocaleLowerCase()
+            .includes(searchValue);
+        })
+        .map((post) => {
+          return <ImagePostEntry imagePost={post} editable={postsEditable} />;
+        })}
+    </Box>
+  );
+};
 
-export default ImagePostBlog
+export default ImagePostBlog;
