47b23ab09d8d7bc1fc4fd203bff8455c4002f974
Kunfi <gian-luca.kunfermann@noseryoung.ch>
2022-11-22 09:09:05 +0100

    removed backend from repository

--- backend/src/main/java/com/example/demo/core/generic/ExtendedRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.core.generic;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExtendedRepository<T extends ExtendedEntity> extends JpaRepository<T, UUID> {
-}
