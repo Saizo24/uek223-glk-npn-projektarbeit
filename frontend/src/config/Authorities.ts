03731c85e388a2771322fdb6458cbfd32b82be3f
Kunfi <gian-luca.kunfermann@noseryoung.ch>
2022-11-22 09:08:38 +0100

    removed frontend from project

--- frontend/src/config/Authorities.ts
+++
@@ -1,9 +0,0 @@
-enum authorities {
-  DEFAULT = "DEFAULT",
-  READ = "READ",
-  USER_MODIFY = "USER_MODIFY",
-  USER_DELETE = "USER_DELETE",
-  USER_CREATE = "USER_CREATE"
-}
-
-export default authorities;
